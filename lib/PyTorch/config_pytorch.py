ab5978fb2086ce9c25379ad05e3c39bfa5dce616
ikiskin <ivan.kiskin@wadh.ox.ac.uk>
2021-07-30 12:44:09 +0100

    PyTorch multiclass support and folder restructure

---
+++ lib/PyTorch/config_pytorch.py
@@ -0,0 +1,12 @@
+# Settings for binary classification for main.ipynb
+
+lr = 0.0015
+max_overrun = 30
+epochs = 50
+batch_size = 32
+pretrained = True
+
+
+# Settings for multi-class classification with 8 species for species_classification.ipynb
+n_classes = 8
+epochs = 100
